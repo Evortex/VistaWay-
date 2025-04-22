e8df18820008940be4133e0d76f28b788f2c48bd
Evortex <codex03@mail.ru>
2025-04-22 20:00:16 +0700

    Реализация рабочего модуля Config Server

--- config-server/src/test/java/com/example/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/example/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.example;
+package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class ConfigServerApplicationTests {
 
     @Test
     void contextLoads() {
