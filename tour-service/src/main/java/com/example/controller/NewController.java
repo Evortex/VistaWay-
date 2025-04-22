9583ca62f6d7153a5fc36b80bff18e94f24c2792
Evortex <codex03@mail.ru>
2025-04-22 21:24:46 +0700

    Реализация рабочего модуля бизнес-логики Tour Service

---
+++ tour-service/src/main/java/com/example/controller/NewController.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import com.example.dto.TourDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new")
+public class NewController {
+
+    @GetMapping("/name")
+    public String name() {
+        return "New Controller";
+    }
+
+    @GetMapping("/tour/{tourId}")
+    public ResponseEntity<TourDto> getTour(@PathVariable("tourId") String tourId) {
+
+        return ResponseEntity.ok(TourDto.builder()
+                .id(tourId)
+                .tourName("VisitCastle")
+                .contactName("Nikolay")
+                .contactPhone("11111111111")
+                .build());
+    }
+}
---
+++ tour-service/src/main/java/com/example/service/TourService.java
@@ -0,0 +1,26 @@
+package com.example.service;
+
+import com.example.model.Tour;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TourService {
+
+    @Value("${topic.send-tour}")
+    private String sendClientTopic;
+
+    private final KafkaTemplate<String , Object> kafkaTemplate;
+
+    public void sendTourEvent(Tour tour) {
+
+        log.info("tour service started");
+
+        kafkaTemplate.send(sendClientTopic, tour);
+    }
+}
